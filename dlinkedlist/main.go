de111152431d0fdffac1fcdf2eaf7257a7ae6cdf
kamyar-nemati <kamyarnemati@gmail.com>
2017-12-06 17:27:50 +0800

    Basic functions such as init and move implemented

--- dlinkedlist/main.go
+++ dlinkedlist/main.go
@@ -6,8 +6,14 @@ type node struct {
 	next *node
 }
 
+func (n *node) construct() {
+	(*n).last = nil
+	(*n).data = nil
+	(*n).next = nil
+}
+
 type iList interface {
-	init() *node
+	init(v interface{}) *node
 	goFirst() *node
 	goLast() *node
 	moveFront() *node
@@ -21,6 +27,45 @@ type list struct {
 	ptr   *node
 	first *node
 	last  *node
+}
+
+func (l *list) init(v interface{}) {
+	if (*l).count != 0 {
+		return
+	}
+
+	newList := new(node)
+
+	(*newList).construct()
+	(*newList).data = v
+
+	(*l).ptr = newList
+	(*l).first = newList
+	(*l).last = newList
+}
+
+func (l *list) goFirst() {
+	(*l).ptr = (*l).first
+}
+
+func (l *list) goLast() {
+	(*l).ptr = (*l).last
+}
+
+func (l *list) moveFront() {
+	if (*(*l).ptr).next == nil {
+		return
+	}
+
+	(*l).ptr = (*(*l).ptr).next
+}
+
+func (l *list) moveBack() {
+	if (*(*l).ptr).last == nil {
+		return
+	}
+
+	(*l).ptr = (*(*l).ptr).last
 }
 
 func main() {
